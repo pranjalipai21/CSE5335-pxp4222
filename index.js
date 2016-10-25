32334ac48b338340e045c46594ea1b30763d4286
pranjalipai21 <pranjali.pai21@gmail.com>
2016-10-25 13:27:29 -0500

    Add existing file

---
+++ index.js
@@ -0,0 +1,78 @@
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5000/db');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  response.render('pages/index');
+});
+
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
+
+app.get('/times', function(request, response) {
+    var result = ''
+    var times = process.env.TIMES || 5
+    for (i=0; i < times; i++)
+      result += i + ' ';
+  response.send(result);
+});
+
+var pg = require('pg');
+var URL = "postgres://qzktucyglfzdac:F-JD5sKSz4YGesEXLvoXqUGDS0@ec2-54-235-254-199.compute-1.amazonaws.com:5432/df91hectgj13ib?ssl=true";
+
+app.get('/dbc', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+	var query = client.query('SELECT * FROM tab_one');
+     query.on("row", function (row,result){
+      result.addRow(row);
+      });
+	  query.on("end", function (result){
+			rowResult= JSON.stringify(result.rows);
+	  client.end();
+	  return response.send(rowResult);
+	  });
+  });
+});
+
+var fs = require('fs');
+app.get('/db', function(request, response) {
+var contents = fs.readFileSync('data.json', 'utf8');
+//console.log(contents);
+response.type('text/json');
+return response.send(contents)
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
---
+++ public/js/main.js
@@ -0,0 +1,75 @@
+$(document).ready(function() {
+	$('#myButton').on('click', function(event) {
+        event.preventDefault();
+        $.ajax({
+            url: "http://localhost:5000/db",
+            type: "GET",
+            contentType: "application/json; charset=utf-8",
+            dataType: 'json',
+            success: function(data) {
+						
+			    $("#tableData").append('<tr ><th style ="text-align: center;">Rank</th><th style ="text-align: center;">State</th><th style ="text-align: center;">Stadium</th><th style ="text-align: center;">Capacity</th></tr>');
+				$.each(data, function(index, value){
+				var row = $("<tr><td>"+value.rank+ "</td><td>"+ value.state+"</td><td>"+value.stadium+"</td><td>"+value.capacity+"</td></tr>");
+				$("#tableData").append(row);
+				
+			});
+			google.charts.load('current', {'packages':['corechart']});
+		    google.charts.setOnLoadCallback(drawChart);
+			markMap();
+            },
+		});
+		
+			$('#myButton').hide();
+    });
+});	
+	
+	
+
+function drawChart(){
+	
+	$.getJSON('http://localhost:5000/db',function(data){
+		var dataTable = new google.visualization.DataTable();
+	 dataTable.addColumn('string','Stadium');
+	 dataTable.addColumn('number','Capacity');
+	 var row_array = [];
+		 $.each(data, function(key, value)
+		 {
+			var each_array = [value.stadium,value.capacity]
+			row_array.push(each_array);
+		 });
+		 dataTable.addRows(row_array);
+	 var options = {'title':'Capacity of the people in the stadium',
+                       'width':550,
+                       'height':500};
+	var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+
+	chart.draw(dataTable, options);
+	 });
+	 
+}
+function markMap(){
+var map = new google.maps.Map(document.getElementById('map'), {
+				  zoom: 4,
+				  center: new google.maps.LatLng(37.7599446, -100.4212681),
+				  mapTypeId: google.maps.MapTypeId.ROADMAP,
+				  scrollwheel: false
+				 });
+				
+		var infowindow = new google.maps.InfoWindow();
+        $.getJSON('http://localhost:5000/db',function(data){
+		
+        	 $.each(data, function(key, value)
+		 {
+			latLng = new google.maps.LatLng(value.latitude, value.longitude);
+            var marker = new google.maps.Marker({
+		    position: latLng,
+			map:map,
+			title: value.stadium
+           });
+		   });
+		   
+
+});
+}
+    
