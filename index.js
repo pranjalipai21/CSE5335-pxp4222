323e54dd3ae4aa327220ea843e061a13b399f5f8
pranjalipai21 <pranjali.pai21@gmail.com>
2016-10-25 19:18:28 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-var cool = require('cool-ascii-faces');
 var express = require('express');
 var app = express();
 
@@ -32,18 +31,6 @@ app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
   response.render('pages/index');
-});
-
-app.get('/cool', function(request, response) {
-  response.send(cool());
-});
-
-app.get('/times', function(request, response) {
-    var result = ''
-    var times = process.env.TIMES || 5
-    for (i=0; i < times; i++)
-      result += i + ' ';
-  response.send(result);
 });
 
 var pg = require('pg');
